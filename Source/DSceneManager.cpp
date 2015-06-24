5070b2dc6a533f4e2de654105adf2876a3c5e24e
SkyZH <iSkyZH@gmail.com>
2015-06-24 10:58:01 +0800

    Simplify SceneManager

--- Source/DSceneManager.cpp
+++ Source/DSceneManager.cpp
@@ -3,156 +3,94 @@
 #include "DScene.h"
 
 DSceneManager::DSceneManager()
-	: PopFlag(0)
-	, ClearFlag(false)
+	:CurrentScene(nullptr),
+	lstScene(nullptr)
 {
 }
 
 
 DSceneManager::~DSceneManager()
 {
-	this->_Clear();
+	this->Clear();
 }
 
 
 void DSceneManager::PullReference(const DawnEngineObject* Object)
 {
 	DawnEngineObject::PullReference(Object);
-	DScene *CurrentScene = this->GetCurrentScene();
 	if (CurrentScene)CurrentScene->PullReference(this);
 }
 
-
 void DSceneManager::Update()
 {
-	this->ProcessPopScene();
-	this->ProcessPushScene();
-	DScene* CurrentScene = this->GetCurrentScene();
-	if (CurrentScene)
+	CurrentScene = this->GetCurrentScene();
+	if (CurrentScene != lstScene)
 	{
-		CurrentScene->Update();
+		ProcessEnd(lstScene);
+		ProcessStart(CurrentScene);
+		lstScene = CurrentScene;
 	}
+	if (CurrentScene)CurrentScene->Update();
 }
-
 
 void DSceneManager::Render()
 {
-	DScene* CurrentScene = this->GetCurrentScene();
-	if (CurrentScene)
-	{
-		CurrentScene->Render();
-	}
+	if (CurrentScene)CurrentScene->Render();
 }
 
+template <class SceneClass> void PushScene()
+{
+	this->SceneQueue.push(dynamic_cast<DScene*>(new SceneClass));
+}
+
+template <class SceneClass> void GotoScene()
+{
+	this->Clear();
+	this->SceneQueue.push(dynamic_cast<DScene*>(new SceneClass));
+}
 
 void DSceneManager::PopScene()
 {
-	if (!PushQueue.empty())
-	{
-		PushQueue.pop();
-	}
-	else
-	{
-		this->PopFlag++;
-	}
+	DScene* Target = this->SceneQueue.back();
+	this->SceneQueue.pop_back();
+	DDel(Target);
 }
-
-
 void DSceneManager::Clear()
 {
-	this->ClearFlag = true;
+	while (!this->isEmpty())this->PopScene();
 }
-
-
-void DSceneManager::ProcessPopScene()
-{
-	while (this->PopFlag > 0)
-	{
-		this->_PopScene();
-		this->PopFlag--;
-	}
-}
-
-
-void DSceneManager::_PopScene()
-{
-	if (!this->Scenes.empty())
-	{
-		this->Scenes.pop_back();
-	}
-}
-
-
-void DSceneManager::_NewScene(DScene* Scene)
-{
-	this->Scenes.push_back(Scene);
-}
-
-
-void DSceneManager::ProcessStart(DScene* Scene)
-{
-	Scene->PullReference(this);
-	Scene->Start();
-}
-
-
-void DSceneManager::ProcessEnd(DScene* Scene)
-{
-	Scene->End();
-}
-
-
-DScene* DSceneManager::GetCurrentScene() const
-{
-	if (Scenes.empty())return nullptr;
-	else return this->Scenes.back();
-}
-
-
-void DSceneManager::ProcessPushScene()
-{
-	DScene* CurrentScene = this->GetCurrentScene();
-	if (this->ClearFlag || !this->PushQueue.empty())
-	{
-		if (CurrentScene)ProcessEnd(CurrentScene);
-
-		if (this->ClearFlag)
-		{
-			this->_Clear();
-		}
-		else if (!this->PushQueue.empty())
-		{
-			DDel(this->GetCurrentScene());
-			_PopScene();
-		}
-		while (!PushQueue.empty())
-		{
-			this->Scenes.push_back(PushQueue.back());
-			PushQueue.pop();
-		}
-		CurrentScene = this->GetCurrentScene();
-		if (CurrentScene)ProcessStart(CurrentScene);
-	}
-}
-
-
-void DSceneManager::_Clear()
-{
-	while (!Scenes.empty())
-	{
-		delete Scenes.back();
-		Scenes.pop_back();
-	}
-}
-
 
 bool DSceneManager::isEmpty() const
 {
-	return this->Scenes.empty() && this->PushQueue.empty();
+	return this->SceneQueue.empty();
 }
 
+DScene* DSceneManager::GetCurrentScene() const
+{
+	return this->SceneQueue.empty() ? nullptr : this->SceneQueue.back();
+}
 
 void DSceneManager::PushSceneObject(DScene* Scene)
 {
-	this->PushQueue.push(Scene);
+	this->SceneQueue.push_back(Scene);
+}
+
+void DSceneManager::GotoSceneObject(DScene *Scene)
+{
+	this->Clear();
+	this->SceneQueue.push_back(Scene);
+}
+
+void DSceneManager::ProcessStart(DScene *Scene)
+{
+	if (Scene)
+	{
+		Scene->PullReference(this);
+		Scene->Start();
+	}
+}
+
+void DSceneManager::ProcessEnd(DScene *Scene)
+{
+	if(Scene)Scene->End();
 }
--- Source/DSceneManager.h
+++ Source/DSceneManager.h
@@ -8,43 +8,37 @@ class DSceneManager :
 {
 	DClass(DSceneManager)
 private:
-	std::vector<DScene*> Scenes;
-	std::queue<DScene*> PushQueue;
+	std::vector<DScene*> SceneQueue;
+	DScene *CurrentScene;
 public:
 	DSceneManager();
 	~DSceneManager();
 	void PullReference(const DawnEngineObject* Object);
 	void Update();
 	void Render();
+
 	template <class SceneClass> void PushScene()
 	{
-		this->PushQueue.push(dynamic_cast<DScene*>(new SceneClass));
+		this->SceneQueue.push_back(dynamic_cast<DScene*>(new SceneClass));
 	}
 
 	template <class SceneClass> void GotoScene()
 	{
-		this->PushQueue.clear();
-		this->ClearFlag = true;
-		this->PushQueue.push(dynamic_cast<DScene*>(new SceneClass));
+		this->Clear();
+		this->SceneQueue.push_back(dynamic_cast<DScene*>(new SceneClass));
 	}
 
 	void PopScene();
 	void Clear();
-private:
-	void ProcessPopScene();
-	int PopFlag;
-	void _PopScene();
-	void _NewScene(DScene* Scene);
-	void ProcessStart(DScene* Scene);
-	void ProcessEnd(DScene* Scene);
-public:
-	DScene* GetCurrentScene() const;
-	void ProcessPushScene();
-private:
-	bool ClearFlag;
-public:
-	void _Clear();
+
 	bool isEmpty() const;
+	DScene* GetCurrentScene() const;
 	void PushSceneObject(DScene* Scene);
+	void GotoSceneObject(DScene *Scene);
+
+private:
+	DScene *lstScene;
+	void ProcessStart(DScene *Scene);
+	void ProcessEnd(DScene *Scene);
 };
 
